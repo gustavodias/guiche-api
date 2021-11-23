13865da98dcd61180bc15baa4e0938d005d67335
gustavodias <gustavoemersonfd@gmail.com>
2021-11-22 23:09:58 -0300

    Commit inicial

---
+++ src/main/java/com/jvge/guicheapi/GuicheApiApplication.java
@@ -0,0 +1,13 @@
+package com.jvge.guicheapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuicheApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuicheApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvge/guicheapi/GuicheApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvge.guicheapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuicheApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
