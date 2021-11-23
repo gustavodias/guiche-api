31c1dc7dea3e3f4872836ae68e6067bf5d11bd7d
gustavodias <gustavoemersonfd@gmail.com>
2021-11-22 23:42:18 -0300

    Configurações do Aplication Properties

--- src/main/java/com/jvge/guicheapi/model/Usuario.java
+++ src/main/java/com/jvge/guicheapi/model/Usuario.java
@@ -1,4 +1,19 @@
 package com.jvge.guicheapi.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
