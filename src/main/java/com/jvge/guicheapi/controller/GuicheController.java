4f4a09bb8c021a1bb9d404e7d9aef0f6cb94a45a
Vitor0liveira <vitoroliveira.ads@gmail.com>
2021-11-23 23:31:02 -0300

    Configuraçao GuicheController com filtro full

--- src/main/java/com/jvge/guicheapi/controller/GuicheController.java
+++ src/main/java/com/jvge/guicheapi/controller/GuicheController.java
@@ -1,10 +1,17 @@
 package com.jvge.guicheapi.controller;
 
+import com.jvge.guicheapi.controller.dto.GuicheDTO;
+import com.jvge.guicheapi.model.Guiche;
 import com.jvge.guicheapi.repository.GuicheRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin("*")
 @RestController
@@ -13,4 +20,12 @@ public class GuicheController {
 
     @Autowired
     private GuicheRepository guicheRepository;
+
+    @GetMapping
+    public ResponseEntity<List<GuicheDTO>> listarGuiche(){
+        List<Guiche> guiches = guicheRepository.findAll();
+        List<GuicheDTO> guicheDTOS = guiches.stream().map(GuicheDTO::new).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(guicheDTOS);
+    }
 }
