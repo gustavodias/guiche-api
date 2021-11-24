4e677d1bdde3ec12ca1310c1619765ba39a0bfa3
gustavodias <gustavoemersonfd@gmail.com>
2021-11-23 22:12:28 -0300

    Criando Usuario Repository

---
+++ src/main/java/com/jvge/guicheapi/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.jvge.guicheapi.repository;
+
+import com.jvge.guicheapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
