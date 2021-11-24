c880cdd1f59a15827901f157e158ecceccb288d6
gustavodias <gustavoemersonfd@gmail.com>
2021-11-23 22:26:31 -0300

    Criação do UsuarioDTO

--- src/main/java/com/jvge/guicheapi/controller/UsuarioController.java
+++ src/main/java/com/jvge/guicheapi/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.jvge.guicheapi.controller;
 
+import com.jvge.guicheapi.model.Usuario;
+import com.jvge.guicheapi.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
-    
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
 }
---
+++ src/main/java/com/jvge/guicheapi/controller/dto/UsuarioDTO.java
@@ -0,0 +1,55 @@
+package com.jvge.guicheapi.controller.dto;
+
+import com.jvge.guicheapi.model.Usuario;
+import org.springframework.data.domain.Page;
+
+public class UsuarioDTO {
+
+    private Long id;
+    private String nomeGuerra;
+    private String setor;
+    private String tipo;
+
+    public UsuarioDTO(Usuario usuario) {
+        this.id = usuario.getId();
+        this.nomeGuerra = usuario.getNomeGuerra();
+        this.setor = usuario.getSetor();
+        this.tipo = usuario.getTipo();
+    }
+
+    public static Page<UsuarioDTO> converte(Page<Usuario> usuarios){
+        return usuarios.map(UsuarioDTO::new);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeGuerra() {
+        return nomeGuerra;
+    }
+
+    public void setNomeGuerra(String nomeGuerra) {
+        this.nomeGuerra = nomeGuerra;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
